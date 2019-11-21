18bbc2236bebc0a9c117cf07860efdec11cf3e0f
LFriday13 <57086419+LFriday13@users.noreply.github.com>
2019-11-21 14:57:55 +0800

    Update GeoNode.cpp

--- GeoNode.cpp
+++ GeoNode.cpp
@@ -8,10 +8,10 @@
 GeoNode::GeoNode()
 : num_dependences(0), parents(nullptr)
 {
-	// TODO Auto-generated constructor stub
+	
 }
 
 GeoNode::~GeoNode() {
-	// TODO Auto-generated destructor stub
+	
 }
 
