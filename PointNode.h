fd2773f485ccbe04a149ac0b19441512369bca34
AJPatupat <44130895+AJPatupat@users.noreply.github.com>
2019-11-22 01:03:28 +0800

    Update PointNode.h

--- PointNode.h
+++ PointNode.h
@@ -8,18 +8,45 @@
 
 #include "GeoNode.h"
 
+enum PointType {
+	INDEPENDENT,
+	ON_LINE,
+	ON_CIRCLE,
+	POINT_POINT_MIDPOINT,
+	LINE_LINE_INTERSECTION,
+	LINE_CIRCLE_INTERSECTION,
+	CIRCLE_CIRCLE_HIGHER_INTERSECTION,
+	CIRCLE_CIRCLE_LOWER_INTERSECTION
+};
+
 class PointNode: public GeoNode {
-	double x, y;
-	void (*construct)();
-	virtual void print() const override;
-	virtual void print_GUI() const override;
-	virtual void access(double data[]) const override;
-	virtual void mutate(double data[]) override;
-	virtual void update() override;
-	void line_line_intersection(double prm[]); // mutator
+
 public:
 	PointNode();
+	PointNode(PointType type, double x, double y);
+	PointNode(PointType type, GeoNode* geo1, double x, double y);
+	PointNode(PointType type, GeoNode* geo1, GeoNode* geo2);
+
 	virtual ~PointNode();
+
+private:
+	double x, y;
+
+	virtual void print() const override;
+	virtual void display() const override;
+
+	virtual void access(double data[]) const override;
+	virtual void mutate(double data[]) override;
+
+	virtual void update() override;
+	void (*update_this)();
+
+	void point_point_midpoint();
+	void line_line_intersection();
+	void line_circle_intersection();
+	void circle_circle_higher_intersection();
+	void circle_circle_lower_intersection();
+
 };
 
 #endif /* POINTNODE_H_ */
