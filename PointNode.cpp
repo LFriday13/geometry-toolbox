8fe8dd411445f8bff17853860f84632a3733df7d
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2019-11-21 21:32:37 +0800

    Add files via upload

--- PointNode.cpp
+++ PointNode.cpp
@@ -3,29 +3,61 @@
  *
  */
 
-#include "PointNode.h"
-
+ class PointNode{
+ 	double x, y;
+ 	void (*construct)();
+ 	void print() const;
+ 	void print_GUI() const;
+ 	void access(double parameters[]) const;
+ 	void mutate(double parameters[]);
+ public:
+ 	PointNode();
+ 	~PointNode();
+ };
 PointNode::PointNode() {
-
+  // TODO
 }
 
 void PointNode::print() const {
-
+  // TODO
 }
 
 void PointNode::print_GUI() const {
-
+  //TODO
 }
 
-void PointNode::access() const {
-
+void PointNode::access(double value[]) const {
+  value[0] = this->x;
+  value[1] = this->y;
 }
 
 void PointNode::mutate() {
-
+  // TODO
 }
 
 PointNode::~PointNode() {
-
+  // TODO
 }
-
+/*
+function name: line_line_intersection
+property: mutator
+parameters:
+prm[0]: line 1 x-coefficient
+prm[1]: line 1 y-coefficient
+prm[2]: line 1 c-coefficient
+prm[3]: line 2 x-coefficient
+prm[4]: line 2 y-coefficient
+prm[5]: line 2 c-coefficient
+implementation sketch: Cramer's rule
+*/
+void line_line_intersection(double prm[]) {
+  double delta, delta_x, delta_y;
+  double x_coeff[2] = {prm[0], prm[3]};
+  double y_coeff[2] = {prm[1], prm[4]};
+  double c_coeff[2] = {prm[2], prm[5]};
+  delta = x_coeff[0] * y_coeff[1] - x_coeff[1] * y_coeff[0];
+  delta_x = c_coeff[0] * y_coeff[1] - c_coeff[1] * y_coeff[0];
+  delta_y = x_coeff[0] * c_coeff[1] - x_coeff[1] * c_coeff[0];
+  x = delta_x / delta;
+  y = delta_y / delta;
+}
