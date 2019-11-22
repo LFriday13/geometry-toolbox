4c0fe7a169d67614e76cea419dea72e8769bfa75
AJPatupat <44130895+AJPatupat@users.noreply.github.com>
2019-11-22 22:50:08 +0800

    Update GeoComponents.cpp

--- GeoComponents.cpp
+++ GeoComponents.cpp
@@ -7,106 +7,94 @@
 
 GeoComponents::GeoComponents() {}
 
-void GeoComponents::add_construction(GeoNode* add){
-	add->pid = next_pid++;
-	geo_components.push_back(add);
+GeoComponents::~GeoComponents() {
+	while(!geo_components.empty()) {
+		delete geo_components.back();
+		geo_components.pop_back();
+	}
 }
 
-void GeoComponents::edit_construction(unsigned int PID, double data[]){ 
-	bool mutated = false;
-	vector <GeoNode*> family;
-	auto it = begin(geo_components); //STL iterator
-	
-	//Locates the element we want to edit
-	for (; it != end(geo_components); ++it){ 
-		if((*it)->pid == PID){
-			if(!(*it)->user_defined){
-				cerr << "The construction is not user defined." << endl;
-				return;
-			}
-			(it == end(geo_components) - 1) ? (*it)->mutate(data): (*it++)->mutate(data);
-			mutated = true;
-			break;
-		}
-	}
-	
-	//Updates every child of the mutated construction (somehow inefficient)
-	while(it != end(geo_components)){ 
-		int num = (*it)->num_parents;
-		const GeoNode* compare = nullptr;
-		bool needs_to_be_updated = false;
-		
-		for(int i=0; i<num; ++i){
-			compare = (*it)->parents[i];
-			for(auto it2 = begin(family); it2 != end(family); ++it2){
-				if(compare == *it2){
-					needs_to_be_updated = true;
+void GeoComponents::add_construction(GeoNode* geo) {
+	geo->pid = next_pid++;
+	geo_components.push_back(geo);
+}
+
+void GeoComponents::edit_construction(unsigned int pid, double data[]) {
+
+	// Record for dependencies
+	bool* is_child = new bool[next_pid];
+
+	// Simultaneous loop through array and vector
+	unsigned int i = 0;
+	for (auto it = begin(geo_components); it != end(geo_components); ++it, ++i){
+
+		if (i < pid) {
+			is_child[i] = false;
+
+		} else if (i == pid) {
+			is_child[i] = true;
+			(*it)->mutate(data);
+
+		} else {
+			is_child[i] = false;
+
+			// Check for dependencies
+			int num = (*it)->num_parents;
+			for (int j = 0; j < num; ++j) {
+				if (is_child[(*it)->parents[j]->pid]) {
+					is_child[i] = true;
 					break;
 				}
 			}
-			if(needs_to_be_updated){break;}
+
+			if (is_child[i])
+				(*it)->update();
 		}
-		
-		if(needs_to_be_updated){
-			(*it)->update();
-		}
-		
-		++it;
 	}
-	
-	//Standard error message
-	if (mutated) {cerr << "Mutated construction " << PID << " succesfully." << endl;}
-	else {cerr << "The construction does not exists." << endl;}
+
+	delete [] is_child;
 }
 
-void GeoComponents::remove_construction(unsigned int PID){
-	bool removed = false; 
-	vector <GeoNode*> family;
-	auto it = begin(geo_components); //STL iterator
-	
-	//Locates the element we want to remove
-	for (; it != end(geo_components); ++it){ 
-		if((*it)->pid == PID){
-			if(!(*it)->user_defined){
-				cerr << "The construction is not user defined." << endl;
-				return;
-			}
-			delete (*it);
-			family.push_back(*it);
-			(it == end(geo_components) - 1) ? geo_components.erase(it): geo_components.erase(it++);
-			removed = true;
-			break;
-		}
-	}
-	
-	//Deletes every child of the deleted construction (somehow inefficient)
-	while(it != end(geo_components)){ 
-		int num = (*it)->num_parents;
-		const GeoNode* compare = nullptr;
-		bool needs_to_be_deleted = false;
-		
-		for(int i=0; i<num; ++i){
-			compare = (*it)->parents[i];
-			for(auto it2 = begin(family); it2 != end(family); ++it2){
-				if(compare == *it2){
-					needs_to_be_deleted = true;
+void GeoComponents::remove_construction(unsigned int pid) {
+
+	// Record for dependencies
+	bool* is_child = new bool[next_pid];
+
+	// Simultaneous loop through array and vector for updating record
+	unsigned int i = 0;
+	for (auto it = begin(geo_components); it != end(geo_components); ++it, ++i) {
+
+		if (i < pid) {
+			is_child[i] = false;
+
+		} else if (i == pid) {
+			is_child[i] = true;
+
+		} else {
+			is_child[i] = false;
+
+			// Check for dependencies
+			int num = (*it)->num_parents;
+			for (int j = 0; j < num; ++j) {
+				if (is_child[(*it)->parents[j]->pid]) {
+					is_child[i] = true;
 					break;
 				}
 			}
-			if(needs_to_be_deleted){break;}
 		}
-		
-		if(needs_to_be_deleted){
-			delete (*it);
-			family.push_back(*it);
-			geo_components.erase(it++);
-		}
-		else{++it;}
 	}
-	
-	//Standard error message
-	if (removed) {cerr << "Deleted construction " << PID << " succesfully." << endl;}
-	else {cerr << "The construction does not exists." << endl;}
+
+	// Reverse simultaneous loop through array and vector for deleting nodes
+	unsigned int i = next_pid - 1;
+	for (auto it = rbegin(geo_components); it != rend(geo_components); ++it, --i) {
+		if (is_child[i]) {
+			delete (*it);
+			geo_components.erase((it+1).base());
+		}
+	}
+
+	delete [] is_child;
+
 }
 
 void GeoComponents::print_all_constructions(){
@@ -118,13 +106,7 @@ void GeoComponents::print_all_constructions(){
 
 void GeoComponents::display_all_constructions(){
 	for (auto it = begin(geo_components); it != end(geo_components); ++it){
-		if((*it)->need_display){(*it)->display();}
-	}
-}
-
-GeoComponents::~GeoComponents() {
-	while(!geo_components.empty()) {
-        	delete geo_components.back();
-        	geo_components.pop_back();
+		if((*it)->need_display)
+			(*it)->display();
 	}
 }
