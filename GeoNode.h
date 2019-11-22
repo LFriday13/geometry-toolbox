6cba2397ccbf26a7fce7d75b9cab31cddcb18bd7
AJPatupat <44130895+AJPatupat@users.noreply.github.com>
2019-11-22 14:07:17 +0800

    Update GeoNode.h

--- GeoNode.h
+++ GeoNode.h
@@ -1,36 +1,43 @@
 /*
- * Geonode.h
+ * GeoNode.h
  *
  */
 
 #ifndef GEONODE_H_
 #define GEONODE_H_
+
 #include <iostream>
 using namespace std;
 
 class GeoNode {
+	friend class GeoComponents;
 
 public:
 	GeoNode();
-	friend class GeoComponents;
-	virtual void access(double data[]) const = 0;
-	
 	virtual ~GeoNode();
-	
-private: 
+
+	virtual void access(double data[]) const = 0;
+
+	void set_pid(unsigned int pid);
+	void set_label(string label);
+	void toggle_display();
+	void not_user_defined();
+
+private:
 	virtual void display() const = 0;
 	virtual void print() const = 0;
 	virtual void mutate(double data[]) = 0;
 	virtual void update() = 0;
-	
+
 protected:
-	unsigned int pid;
-	bool user_defined;
-	bool hidden;
-	string label;
-	const int num_parents;
-	const GeoNode** parents;
-	
+	unsigned int pid {0};
+	string label {""};
+	bool user_defined {true};
+	bool need_display {true};
+
+	const int num_parents {0};
+	const GeoNode** parents {nullptr};
+
 };
 
 #endif /* GEONODE_H_ */
