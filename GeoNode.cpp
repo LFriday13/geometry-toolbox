c3b78685de5bf787b62c56e25e338cee7549a112
LFriday13 <57086419+LFriday13@users.noreply.github.com>
2019-11-22 11:37:18 +0800

    Updated GeoNode.h

--- GeoNode.cpp
+++ GeoNode.cpp
@@ -5,11 +5,7 @@
 
 #include "GeoNode.h"
 
-GeoNode::GeoNode()
-: num_dependences(0), parents(nullptr)
-{
-	
-}
+GeoNode::GeoNode() : num_parents(0), parents(nullptr) {}
 
 GeoNode::~GeoNode() {
 	
--- GeoNode.h
+++ GeoNode.h
@@ -9,21 +9,28 @@
 using namespace std;
 
 class GeoNode {
-	unsigned int pid;
-	bool user_defined;
-	bool hidden;
-	string label;
-	const int num_dependences;
-	const GeoNode** parents;
+	
+public:
+	GeoNode();
+	friend class GeoComponents;
+	
+	virtual ~GeoNode();
+	
+private: 
 	virtual void display() const = 0;
 	virtual void print() const = 0;
 	virtual void access(double data[]) const = 0;
 	virtual void mutate(double data[]) = 0;
 	virtual void update() = 0;
-public:
-	GeoNode();
-	virtual ~GeoNode();
-	friend class GeoComponents;
+	
+protected:
+	unsigned int pid;
+	bool user_defined;
+	bool hidden;
+	string label;
+	const int num_parents;
+	const GeoNode** parents;
+	
 };
 
 #endif /* GEONODE_H_ */
