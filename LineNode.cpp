1782876bacba34fce146a0b45e25b2749d6b853f
AJPatupat <44130895+AJPatupat@users.noreply.github.com>
2019-11-23 02:13:31 +0800

    Update LineNode.cpp

--- LineNode.cpp
+++ LineNode.cpp
@@ -3,33 +3,71 @@
  *
  */
 
-#include "LineNode.h"
+LineNode::LineNode() {}
 
-LineNode::LineNode() {
+LineNode::LineNode(LineType type, GeoNode* geo1, GeoNode* geo2)  : GeoNode(2) {
 
+	switch(type) {
+		case POINT_POINT_LINE_THROUGH: definition = point_point_line_through; break;
+		case POINT_LINE_PARALLEL_LINE_THROUGH: definition = point_line_parallel_line_through; break;
+		case POINT_POINT_PERPENDICULAR_BISECTOR: definition = point_point_perpendicular_bisector; break;
+		case POINT_CIRCLE_HIGHER_TANGENT: definition = point_circle_higher_tangent; break;
+		case POINT_CIRCLE_LOWER_TANGENT: definition = point_circle_lower_tangent; break;
+	}
+
+	parents = new const GeoNode*[num_parents];
+	parents[0] = geo1;
+	parents[1] = geo2;
+	(this->*definition)();
 }
 
-void LineNode::print() const {
+LineNode::~LineNode() {}
 
+void LineNode::print() const {
+	//TODO
 }
 
 void LineNode::display() const {
-
+	//TODO
 }
 
 void LineNode::access(double data[]) const {
-
+	data[0] = x_coeff;
+	data[1] = y_coeff;
+	data[2] = c_coeff;
 }
 
 void LineNode::mutate(double data[]) {
-
+	x_coeff = data[0];
+	y_coeff = data[1];
+	c_coeff = data[2];
+	(this->*definition)();
 }
 
-void LineNode::update(){
-	
+void LineNode::update() {
+	(this->*definition)();
 }
 
-LineNode::~LineNode() {
-	
+void LineNode::point_point_line_through() {
+	//TODO
 }
 
+void LineNode::point_line_parallel_line_through() {
+	//TODO
+}
+
+void LineNode::point_point_perpendicular_bisector() {
+	//TODO
+}
+
+void LineNode::point_circle_higher_tangent() {
+	//TODO
+	//If the point is inside the circle, set well_defined as false
+	//Otherwise, calculate the tangent normally
+}
+
+void LineNode::point_circle_lower_tangent() {
+	//TODO
+	//If the point is inside or on the circle, set well_defined as false
+	//Otherwise, calculate the tangent normally
+}
