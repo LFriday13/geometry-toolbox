3108a29ccf0a009959339c64feee13840a9b6df2
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2019-11-26 18:39:03 +0800

    Update TriangleNode.cpp

--- TriangleNode.cpp
+++ TriangleNode.cpp
@@ -7,14 +7,14 @@
 TriangleNode::TriangleNode() {}
 
 TriangleNode::TriangleNode(TriangleType type, GeoNode* geo1, GeoNode* geo2, GeoNode* geo3): GeoNode(3) {
-  switch(type) {
+  	switch(type) {
 		case POINT_POINT_POINT_VERTICES: definition = &TriangleNode::point_point_point_vertices; break;
 		case LINE_LINE_LINE_INTERSECTIONS: definition = &TriangleNode::line_line_line_intersections; break;
 	}
-  parents = new const GeoNode*[num_parents];
+  	parents = new const GeoNode*[num_parents];
 	parents[0] = geo1;
 	parents[1] = geo2;
-  parents[2] = geo3;
+  	parents[2] = geo3;
 	(this->*definition)();
 }
 
@@ -27,9 +27,9 @@ void TriangleNode::display() const {
 }
 
 void TriangleNode::access(double data[]) const {
-  data[0] = side_a;
-  data[1] = side_b;
-  data[2] = side_c;
+  	data[0] = side_a;
+  	data[1] = side_b;
+  	data[2] = side_c;
 }
 
 void TriangleNode::mutate(double data[]) {
@@ -37,7 +37,7 @@ void TriangleNode::mutate(double data[]) {
 }
 
 void TriangleNode::update() {
-  (this->*definition)();
+  	(this->*definition)();
 }
 
 TriangleNode::~TriangleNode() {}
