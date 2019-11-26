c088924644b58222907008ed7537b933eaa49828
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2019-11-26 18:37:37 +0800

    Add files via upload

--- TriangleNode.cpp
+++ TriangleNode.cpp
@@ -2,11 +2,20 @@
  * TriangleNode.cpp
  *
  */
-
 #include "TriangleNode.h"
 
-TriangleNode::TriangleNode() {
+TriangleNode::TriangleNode() {}
 
+TriangleNode::TriangleNode(TriangleType type, GeoNode* geo1, GeoNode* geo2, GeoNode* geo3): GeoNode(3) {
+  switch(type) {
+		case POINT_POINT_POINT_VERTICES: definition = &TriangleNode::point_point_point_vertices; break;
+		case LINE_LINE_LINE_INTERSECTIONS: definition = &TriangleNode::line_line_line_intersections; break;
+	}
+  parents = new const GeoNode*[num_parents];
+	parents[0] = geo1;
+	parents[1] = geo2;
+  parents[2] = geo3;
+	(this->*definition)();
 }
 
 void TriangleNode::print() const {
@@ -18,7 +27,9 @@ void TriangleNode::display() const {
 }
 
 void TriangleNode::access(double data[]) const {
-
+  data[0] = side_a;
+  data[1] = side_b;
+  data[2] = side_c;
 }
 
 void TriangleNode::mutate(double data[]) {
@@ -26,10 +37,15 @@ void TriangleNode::mutate(double data[]) {
 }
 
 void TriangleNode::update() {
+  (this->*definition)();
+}
+
+TriangleNode::~TriangleNode() {}
+
+void TriangleNode::point_point_point_vertices() {
 
 }
 
-TriangleNode::~TriangleNode() {
-	
+void TriangleNode::line_line_line_intersections() {
+  
 }
-
--- TriangleNode.h
+++ TriangleNode.h
@@ -8,16 +8,25 @@
 
 #include "GeoNode.h"
 
+enum TriangleType {
+	POINT_POINT_POINT_VERTICES,
+  LINE_LINE_LINE_INTERSECTIONS
+};
+
 class TriangleNode: public GeoNode {
 	double side_a, side_b, side_c;
-	void (*construct)();
+	void (TriangleNode::*definition)() {nullptr};
 	virtual void print() const override;
 	virtual void display() const override;
 	virtual void access(double data[]) const override;
 	virtual void mutate(double data[]) override;
 	virtual void update() override;
+
+  void point_point_point_vertices();
+  void line_line_line_intersections();
 public:
-	TriangleNode();
+  TriangleNode();
+	TriangleNode(TriangleType type, GeoNode* geo1, GeoNode* geo2, GeoNode* geo3);
 	virtual ~TriangleNode();
 };
 
