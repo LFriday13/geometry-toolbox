8f251cc770763be7925ab08ea8c2a45b90e91c61
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2019-12-03 12:40:35 +0800

    Update TriangleNode.h

--- TriangleNode.h
+++ TriangleNode.h
@@ -10,10 +10,13 @@
 
 enum TriangleType {
 	POINT_POINT_POINT_VERTICES,
-  LINE_LINE_LINE_INTERSECTIONS
+  	LINE_LINE_LINE_INTERSECTIONS
 };
 
 class TriangleNode: public GeoNode {
+	friend class: TriangleCentersNode;
+	
+	private:
 	double side_a, side_b, side_c;
 	void (TriangleNode::*definition)() {nullptr};
 	virtual void print() const override;
@@ -22,12 +25,13 @@ class TriangleNode: public GeoNode {
 	virtual void mutate(double data[]) override;
 	virtual void update() override;
 
-  void point_point_point_vertices();
-  void line_line_line_intersections();
+	void point_point_point_vertices();
+	void line_line_line_intersections();
 
-  void get_intersection_helper(double line1[], double line2[], double point[]);
-public:
-  TriangleNode();
+	void get_intersection_helper(double line1[], double line2[], double point[]);
+	
+	public:
+	TriangleNode();
 	TriangleNode(TriangleType type, GeoNode* geo1, GeoNode* geo2, GeoNode* geo3);
 	virtual ~TriangleNode();
 };
