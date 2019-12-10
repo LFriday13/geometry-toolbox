8e3ed8e042f56ad7a610b585c4b620592c455add
gkdso738 <54734860+gkdso738@users.noreply.github.com>
2019-12-10 12:21:53 +0800

    solve the unexpected exit

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@
 
 int main(int argc, char *argv[])
 {
-    GeoComponents geo_components;
+    GeoComponents* geo_components = new GeoComponents;
 
     // Demo Configuration
     int i = 1;
-    for(; i<11; ++i){geo_components.add_construction(new PointNode(INDEPENDENT, i, i*i), "demo_"+std::to_string(i-1));}
-    geo_components.add_construction(new CircleNode(POINT_POINT_POINT_THROUGH, geo_components.get_construction(0), geo_components.get_construction(1), geo_components.get_construction(2)),  "demo_"+std::to_string(i++ -1));
-    geo_components.add_construction(new LineNode(POINT_CIRCLE_FIRST_TANGENT, geo_components.get_construction(5), geo_components.get_construction(10)),  "demo_"+std::to_string(i++ -1));
-    geo_components.add_construction(new LineNode(POINT_CIRCLE_SECOND_TANGENT, geo_components.get_construction(5), geo_components.get_construction(10)),  "demo_"+std::to_string(i++ -1));
-    geo_components.add_construction(new PointNode(INDEPENDENT, -10, 0), "demo_"+std::to_string(i++ -1));
-    geo_components.add_construction(new TriangleNode(POINT_POINT_POINT_VERTICES, geo_components.get_construction(0), geo_components.get_construction(1), geo_components.get_construction(13)),  "demo_"+std::to_string(i++ -1));
+    for(; i<11; ++i){geo_components->add_construction(new PointNode(INDEPENDENT, i, i*i), "demo_"+std::to_string(i-1));}
+    geo_components->add_construction(new CircleNode(POINT_POINT_POINT_THROUGH, geo_components->get_construction(0), geo_components->get_construction(1), geo_components->get_construction(2)),  "demo_"+std::to_string(i++ -1));
+    geo_components->add_construction(new LineNode(POINT_CIRCLE_FIRST_TANGENT, geo_components->get_construction(5), geo_components->get_construction(10)),  "demo_"+std::to_string(i++ -1));
+    geo_components->add_construction(new LineNode(POINT_CIRCLE_SECOND_TANGENT, geo_components->get_construction(5), geo_components->get_construction(10)),  "demo_"+std::to_string(i++ -1));
+    geo_components->add_construction(new PointNode(INDEPENDENT, -10, 0), "demo_"+std::to_string(i++ -1));
+    geo_components->add_construction(new TriangleNode(POINT_POINT_POINT_VERTICES, geo_components->get_construction(0), geo_components->get_construction(1), geo_components->get_construction(13)),  "demo_"+std::to_string(i++ -1));
 
     // Application Setup
     QApplication a(argc, argv);
-    MainWindow w(&geo_components);
+    MainWindow w(geo_components);
     w.show();
     return a.exec();
 }
